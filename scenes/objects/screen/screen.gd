5893aa50ba921243dc5571b69f412019de74d96c
Trent <trentthebaker@gmail.com>
2025-04-06 15:59:08 -0300

    Add screen volume attenuation

--- scenes/objects/screen/screen.gd
+++ scenes/objects/screen/screen.gd
@@ -1,22 +1,40 @@
 extends Node3D
 
 @export var stream_player : VideoStreamPlayer
+@export var audio_origin_node : Node3D
+@export var volume_attenuation_curve : Curve
+@export var max_attenuation_distance : float
 
 var _playback_pause_timer : Timer
 var _previously_loaded_stream_position : float
+
 
 func _ready():
 	play_at_position(500.0) # Debug stuff, delete me
 	_playback_pause_timer = Timer.new()
 	_playback_pause_timer.timeout.connect(_on_playback_pause_timer_timeout)
 	add_child(_playback_pause_timer)
-	# stream_player.volume = 0
+	
+	ReplayManager.started_playback.connect(_on_replay_manager_start_playback)
+
+
+func _on_replay_manager_start_playback() -> void:
+	stream_player.bus = "CameraRecordedPlayback"
+
+
+func _process(_delta: float) -> void:
+	var active_camera = get_viewport().get_camera_3d()
+	var distance_to_camera = (audio_origin_node.global_position - active_camera.global_position).length()
+	var distance_percentage = remap(distance_to_camera, 0, max_attenuation_distance, 0, 1)
+	stream_player.volume = volume_attenuation_curve.sample(distance_percentage)
+
 
 func _save_state() -> Dictionary:
 	var state = {}
 	state.stream_position = stream_player.stream_position
 	return state
 	
+
 func _load_state(state : Dictionary) -> void:
 	if stream_player.paused or abs(stream_player.stream_position - _previously_loaded_stream_position) >= ReplayManager.get_tick_rate() * 2:
 		# print("setting stream position to %f!" % [state.stream_position])
@@ -27,6 +45,7 @@ func _load_state(state : Dictionary) -> void:
 	_playback_pause_timer.start(ReplayManager.get_tick_rate() * 2)
 
 	_previously_loaded_stream_position = state.stream_position
+
 
 func _on_playback_pause_timer_timeout() -> void:
 	set_paused(true)
