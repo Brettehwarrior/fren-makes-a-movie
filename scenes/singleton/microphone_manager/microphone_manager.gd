e638bdf4694371404c1d4529af9a781a70e92faf
Trent <trentthebaker@gmail.com>
2025-04-05 23:44:26 -0300

    Get rolling average of mic volume

--- scenes/singleton/microphone_manager/microphone_manager.gd
+++ scenes/singleton/microphone_manager/microphone_manager.gd
@@ -1,12 +1,25 @@
 extends Node
 
-
 @export var mic_volume_progress_bar : ProgressBar
 
+@export_range(1, 30) var samples_for_current_volume : int
+
 var _current_mic_volume : float
+var _recent_mic_volumes : Array[float] = []
 
 func _process(_delta):
 	var volume_db = AudioServer.get_bus_peak_volume_left_db(AudioServer.get_bus_index("Record"), 0)
 	_current_mic_volume = db_to_linear(volume_db)
-	mic_volume_progress_bar.value = _current_mic_volume
-	print(_current_mic_volume)
+	_recent_mic_volumes.push_back(_current_mic_volume)
+
+	if len(_recent_mic_volumes) > samples_for_current_volume:
+		_recent_mic_volumes.pop_front()
+
+	mic_volume_progress_bar.value = _get_average(_recent_mic_volumes)
+	
+func _get_average(list : Array[float]) -> float:
+	var average = 0
+	for item in list:
+		average += item
+	average /= len(list)
+	return average
