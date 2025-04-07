2b3a39e55e773539e26c3811bcba39c685125302
Andrew <41998850+andrewplus@users.noreply.github.com>
2025-04-07 20:23:07 -0300

    Viewfinder follow fren :)

--- scenes/objects/camcorder/camcorder.gd
+++ scenes/objects/camcorder/camcorder.gd
@@ -2,6 +2,9 @@ class_name Camcorder
 extends Node3D
 
 @export var camera : Node3D
+@export var viewfinder : Node3D
+@export var viewport : Node3D
+@export var fren : Node3D
 
 var _parent_node : Node3D
 var _original_parent_node : Node
@@ -13,14 +16,30 @@ func _ready() -> void:
 	_original_parent_node = get_parent()
 	_parent_node = _original_parent_node
 
-
 func _on_replay_manager_started_playback() -> void:
 	reset_follow_nodes()
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	camera.rotation = global_rotation
+	_viewfinder_follow_player(delta)
 
+
+func _viewfinder_follow_player(delta: float) -> void:
+	if not is_held() and fren != null:
+		var dir = to_local(fren.global_transform.origin).normalized()
+		var target_rotation
+		
+		if dir.z < 0:
+			viewport.rotation_degrees.z = 180.0
+			target_rotation = -180.0
+		else:
+			viewport.rotation_degrees.z = 0.0
+			target_rotation = 0.0
+		
+		viewfinder.rotation_degrees.x = lerp(viewfinder.rotation_degrees.x, target_rotation, delta * 10.0)
+	else:
+		viewfinder.rotation_degrees.x = 0
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("camera_record") and not ReplayManager.is_recording():
