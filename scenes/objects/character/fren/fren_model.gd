44d539d125eb9026d333b455b1a13f2f8ec14005
Trent <trentthebaker@gmail.com>
2025-04-12 12:08:43 -0300

    change model to meshes

--- scenes/objects/character/fren/fren_model.gd
+++ scenes/objects/character/fren/fren_model.gd
@@ -5,7 +5,7 @@ extends Node3D
 @export var open_mouth_mic_threshold : float = 0.5
 @export var normal_texture : Texture2D
 @export var speaking_texture : Texture2D
-@export var front_sprite : Sprite3D
+@export var front_sprite_top : MeshInstance3D
 
 
 func _ready() -> void:
@@ -24,28 +24,29 @@ func _on_fren_stopped_walking() -> void:
 
 func _save_state() -> Dictionary:
 	var state = {}
-	state.is_speaking = front_sprite.texture == speaking_texture
+	state.is_speaking = front_sprite_top.get_active_material(0).albedo_texture == speaking_texture
 	return state
 
 
 func _load_state(state : Dictionary) -> void:
 	if state.is_speaking:
-		if front_sprite.texture == normal_texture:
-			front_sprite.texture = speaking_texture
+		if front_sprite_top.get_active_material(0).albedo_texture == normal_texture:
+			front_sprite_top.get_active_material(0).albedo_texture = speaking_texture
 	else:
-		if front_sprite.texture == speaking_texture:
-			front_sprite.texture = normal_texture
+		if front_sprite_top.get_active_material(0).albedo_texture == speaking_texture:
+			front_sprite_top.get_active_material(0).albedo_texture = normal_texture
 
 
 func _process(_delta: float) -> void:
 	if ReplayManager.is_playing_back():
 		return
 	
-	front_sprite.texture = normal_texture
+	var mesh_material = front_sprite_top.get_active_material(0)
+	mesh_material.albedo_texture = normal_texture
 	var mic_volume : float = MicrophoneManager.get_current_mic_volume()
 	if mic_volume >= open_mouth_mic_threshold:
-		if front_sprite.texture == normal_texture:
-			front_sprite.texture = speaking_texture
+		if mesh_material.albedo_texture == normal_texture:
+			mesh_material.albedo_texture = speaking_texture
 	else:
-		if front_sprite.texture == speaking_texture:
-			front_sprite.texture = normal_texture
+		if mesh_material.albedo_texture == speaking_texture:
+			mesh_material.albedo_texture = normal_texture
