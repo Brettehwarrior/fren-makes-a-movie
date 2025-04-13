f92d87a80491bdc20376f9fdf908849433a93b1c
Andrew <41998850+andrewplus@users.noreply.github.com>
2025-04-13 19:43:10 -0300

    More improvements to playback screen

--- scenes/playback/playback_ui/playback_ui.gd
+++ scenes/playback/playback_ui/playback_ui.gd
@@ -3,6 +3,7 @@ extends Control
 @export var video_playback_texture : TextureRect
 
 @export var timeline : Control
+@export var viewcount_field : LineEdit
 
 @export var play_pause_button : Button
 @export var play_icon : Texture2D
@@ -16,6 +17,8 @@ var _is_playing : bool = false
 var _timeline_opacity : float
 var _timeline_hide_cooldown : float = 1.0
 var _timeline_hide_timer : float
+var _viewcount : int = 0
+var _maxviews : int = 1000000000
 
 
 func _ready() -> void:
@@ -25,16 +28,20 @@ func _ready() -> void:
 	
 
 func _process(delta: float) -> void:
-	if ReplayManager.is_playing_back() and timeline.get_global_rect().has_point(timeline.get_global_mouse_position()):
-		_timeline_opacity = 1.0
-		_timeline_hide_timer = _timeline_hide_cooldown
-	else:
-		if _timeline_hide_timer <= 0:
-			_timeline_opacity = 0.0
+	if ReplayManager.is_playing_back():
+		if timeline.get_global_rect().has_point(timeline.get_global_mouse_position()):
+			_timeline_opacity = 1.0
+			_timeline_hide_timer = _timeline_hide_cooldown
 		else:
-			_timeline_hide_timer -= delta
+			if _timeline_hide_timer <= 0:
+				_timeline_opacity = 0.0
+			else:
+				_timeline_hide_timer -= delta
 	
 	timeline.modulate.a = lerp(timeline.modulate.a, _timeline_opacity, delta * 20)
+	_viewcount = lerp(_viewcount, _maxviews, delta * 0.000001)
+	viewcount_field.text = str(_viewcount) + " views"
+
 
 func _on_playback_camera_created(subviewport : SubViewport) -> void:
 	video_playback_texture.texture = subviewport.get_texture()
@@ -44,6 +51,8 @@ func _initialize() -> void:
 	visible = true
 	frame_slider.max_value = ReplayManager.get_current_recording_frame() - 1
 	total_frames_label.text = str(int(frame_slider.max_value))
+	_timeline_opacity = 1.0
+	_timeline_hide_timer = _timeline_hide_cooldown
 	assert(frame_slider.max_value > 0, "ReplayManager has no frames recorded")
 
 
@@ -85,3 +94,11 @@ func _load_game_state(frame : int) -> void:
 func _on_frame_slider_drag_started() -> void:
 	_is_playing = false
 	_update_play_pause_button_icon()
+
+
+func _on_title_screen_pressed() -> void:
+	pass
+
+
+func _on_new_movie_pressed() -> void:
+	pass
