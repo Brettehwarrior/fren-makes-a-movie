9927fe262d976bc4724a3edf5c8c5985aed3c5ad
Trent <trentthebaker@gmail.com>
2025-04-13 22:49:08 -0300

    add button prompts

--- scenes/objects/triggers/interaction/ui/interaction_ui.gd
+++ scenes/objects/triggers/interaction/ui/interaction_ui.gd
@@ -1,15 +1,16 @@
 extends Control
 
+@export var crosshair_parent : Control
 @export var interaction_label : RichTextLabel
 
 func _process(_delta: float) -> void:
 	# Interaction text
 	if InteractionManager.interaction_name != "" and InteractionManager.interaction_keybind != "":
-		interaction_label.text = InteractionManager.interaction_name + " [" + InteractionManager.interaction_keybind + "]"
-		modulate = Color(225, 225, 0, 1)
+		interaction_label.text = "[img={30}]uid://do173ynst785x[/img]" + InteractionManager.interaction_name
+		crosshair_parent.modulate = Color(225, 225, 0, 1)
 	else:
 		interaction_label.text = ""
-		modulate = Color(225, 225, 225, 1)
+		crosshair_parent.modulate = Color(225, 225, 225, 1)
 		
 	# Show or hide entire interaction HUD, including crosshair
 	if SettingsManager.is_showing() or ReplayManager.is_playing_back():
